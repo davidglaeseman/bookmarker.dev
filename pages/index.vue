bfe45d5a89f82f522bb6dc83c8f08f678079a03a
davidglaeseman <davidglaeseman@dglaeseman-mbp.local>
2019-12-04 11:03:57 -0700

    added nuxt default code

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+
+	<div class="index">
+		<h1>{{name}}</h1>
+	</div>
+
+</template>
+
+<style type="text/css" lang="scss">
+	.index {
+
+	}
+</style>
+
+<script>
+	export default {
+		asyncData(){
+			return {
+				title: 'Bookmarker.dev - Bookmarks For People'
+			}
+		},
+		data() {
+			return {
+				name: 'Bookmarker.dev'
+			}
+		},
+		computed: {},
+		mounted() {
+
+		},
+		methods: {
+			example() {
+				return this.name;
+			}
+		},
+		watch: {},
+		head() {
+			return {
+				title: this.name
+			}
+		}
+	}
+</script>
